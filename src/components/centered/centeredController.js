01ce6e2c62d863ac0d87c83d6947ebbbfd499b8f
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-03-14 10:58:55 -0500

    StyFix welcome message

--- src/components/centered/centeredController.js
+++ src/components/centered/centeredController.js
@@ -5,6 +5,6 @@ angular.module('app-bootstrap').controller('centeredController', [
   function () {
     this.logo = require('./../../assets/wolox_logo.svg');
     this.title = 'Welcome to AngularJS Bootstrap!';
-    this.name = 'a AngularJS controller';
+    this.name = 'AngularJS controller';
   }
 ]);
